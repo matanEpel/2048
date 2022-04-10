5a5fb9e5ca52f06853578e2e4370036f17d2c2c7
matanepel <matan.epel12@gmail.com>
2022-04-10 19:29:36 +0300

    finishing evaluations function

--- multi_agents.py
+++ multi_agents.py
@@ -54,40 +54,57 @@ class ReflexAgent(Agent):
         score = successor_game_state.score
 
         """
-        Well, when we started the exercise, we've implemented a basic function
-        that takes into account these three parameters:
-        1. whether the max tile of the successor state is bigger than the current state
-        2. whether the amount of free locations is bigger.
-        3. whether the score is higher.
-        and giving "bonus" points to the evaluation accordingly.
-        However, this approach gave us an average of 187 (max tile),
-        And a distribution of [32: 0.2%, 64: 10%, 128: 45%, 256: 42%, 512: 33%]
-        which is not very good.
-        So, we "commented this function (below).
-        
-        Then, we created the "better_evaluation_function" for the minimax algorithm,
-        and after a lot of thinking it is a much better function. So, we've decided to
-        use it instead.
-        The stats for this function when running the reflex agents are (after running on 1000 games):
-        Average: 268 (max tile)
-        Distribution: [32: 0.2%, 64: 2.9%, 128: 17.8%, 256: 64.4%, 512: 15%, 1024: 0.3%]
-        Which is much better than the previous one
+        DESCRIPTION: The idea behind this evaluation function is, as you suggested - combining some heuristics
+        together in order to create a better one. All the heuristics are normalized to 1 in order to give them the same
+        effect (and then we can multiply each one by a constant in order to give each one a different weight.
+        The three heuristics are:
+        1. "symmetric formation", as an individual who played tons of 2048 games in the past, the best strategy
+        is to create a symmetric formation. Meaning - top left is the highest tile, and as we go away from it
+        the values decrease. For example:
+        64 32 16 0
+        32 16 0  0
+        16 0  0  0
+    
+        2. "free tiles" we want to have a lot of "free tile", so we give score for the amount of
+        free tiles divided by the amount of possible free tiles.
+    
+        3. highest to the corner. The symmetric formation is nice, but we want to keep the highest
+        tile in one of the corners even if when it is not the formation is less symmetric. So, we gave
+        "bonus" points for boards where the highest is in the corner.
+    
         """
-        """
+        "*** YOUR CODE HERE ***"
+        board = successor_game_state.board
+
+        # the symmetric formation we have:
+        scores1 = np.array([[6, 5, 4, 3], [5, 4, 3, 2], [4, 3, 2, 1], [3, 2, 1, 0]])
+        scores2 = np.array([[3, 4, 5, 6], [2, 3, 4, 5], [1, 2, 3, 4], [0, 1, 2, 3]])
+        scores3 = np.array([[3, 2, 1, 0], [4, 3, 2, 1], [5, 4, 3, 2], [6, 5, 4, 3]])
+        scores4 = np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]])
+
+        scores = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6]
+
+        # calculating the max symmetric score:
+        snake = 0
+        for s in [scores1, scores2, scores3, scores4]:
+            snake = max(snake, np.sum(np.multiply(s, board)))
+
+        # normalizing:
+        max_possible = 0
+        data = np.sort(board.flatten())
+        for i in range(0, len(data)):
+            max_possible += data[i] * scores[i]
+        snake_score = snake / max_possible
+
+        # calculating the percentage of free tiles:
+        free_tiles_factor = 15 - np.log2(successor_game_state.max_tile)
+        free_tiles_score = (16 - np.count_nonzero(board)) / free_tiles_factor
+
+        # adding the "max tile to the corner" bonus
         max_bonus = 0
-        if current_game_state.max_tile < max_tile:
+        if max([board[0,0], board[0,3], board[3,0], board[3,3]]) == successor_game_state.max_tile:
             max_bonus = 1
-
-        free_bonus = np.count_nonzero(current_game_state.board) - np.count_nonzero(board) - 1
-
-        score_bonus = 0
-        if score > current_game_state.score:
-            score_bonus = 1
-
-        return score_bonus + free_bonus + max_bonus
-        """
-
-        return better_evaluation_function(successor_game_state)
+        return snake_score + free_tiles_score + max_bonus*2
 
 
 def score_evaluation_function(current_game_state):
@@ -147,7 +164,7 @@ class MinmaxAgent(MultiAgentSearchAgent):
 
         legal_moves = game_state.get_agent_legal_actions()
         # Choose one of the best actions
-        scores = [self.get_action_minimax(game_state.generate_successor(0, action), 0, True) for action in legal_moves]
+        scores = [self.get_action_minimax(game_state.generate_successor(0, action), 0, False) for action in legal_moves]
         best_score = max(scores)
         best_indices = [index for index in range(len(scores)) if scores[index] == best_score]
         chosen_index = np.random.choice(best_indices)  # Pick randomly among the best
@@ -155,18 +172,25 @@ class MinmaxAgent(MultiAgentSearchAgent):
         return legal_moves[chosen_index]
 
     def get_action_minimax(self, game_state: GameState, curDepth, maxTurn):
-        if curDepth == self.depth - 1:
-            legal_moves = game_state.get_agent_legal_actions()
-            scores = [self.evaluation_function(game_state.generate_successor(0, action)) for action in legal_moves]
-            return max(scores)
+        if np.count_nonzero(game_state.board) == 16:
+            return 0
+
+        if curDepth == self.depth - 1 and not maxTurn:
+            legal_moves = game_state.get_opponent_legal_actions()
+            scores = [self.evaluation_function(game_state.generate_successor(1, action)) for action in legal_moves]
+            return min(scores)
 
         if maxTurn:
             legal_moves = game_state.get_agent_legal_actions()
-            scores = [self.get_action_minimax(game_state.generate_successor(0, action), curDepth+1, False) for action in legal_moves]
+            scores = [self.get_action_minimax(game_state.generate_successor(0, action), curDepth, False) for action in
+                      legal_moves]
             return max(scores)
 
         else:
-            return self.get_action_minimax(game_state.generate_successor(1), curDepth, True)
+            legal_moves = game_state.get_opponent_legal_actions()
+            scores = [self.get_action_minimax(game_state.generate_successor(1, action), curDepth+1, True) for action in
+                      legal_moves]
+            return min(scores)
 
 
 
@@ -184,7 +208,7 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         """*** YOUR CODE HERE ***"""
         legal_moves = game_state.get_agent_legal_actions()
         # Choose one of the best actions
-        scores = [self.get_action_alpha_beta(game_state.generate_successor(0, action), 0, True, float("-inf"), float("inf"))
+        scores = [self.get_action_alpha_beta(game_state.generate_successor(0, action), 0, False, float("-inf"), float("inf"))
                   for action in legal_moves]
         best_score = max(scores)
         best_indices = [index for index in range(len(scores)) if scores[index] == best_score]
@@ -193,25 +217,39 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         return legal_moves[chosen_index]
 
     def get_action_alpha_beta(self, game_state: GameState, curDepth, maxTurn, alpha, beta):
-        if curDepth == self.depth - 1:
-            legal_moves = game_state.get_agent_legal_actions()
-            scores = [self.evaluation_function(game_state.generate_successor(0, action)) for action in legal_moves]
-            return max(scores)
+        if np.count_nonzero(game_state.board) == 16:
+            return 0
+
+        if curDepth == self.depth - 1 and not maxTurn:
+            legal_moves = game_state.get_opponent_legal_actions()
+            scores = [self.evaluation_function(game_state.generate_successor(1, action)) for action in legal_moves]
+            return min(scores)
 
         if maxTurn:
-            best_value = float("-inf")
             legal_moves = game_state.get_agent_legal_actions()
+            max_val = np.float('-inf')
             for action in legal_moves:
-                value = self.get_action_alpha_beta(game_state.generate_successor(0, action), curDepth + 1, False, alpha, beta)
-                best_value = max(best_value, value)
-                alpha = max(alpha, best_value)
+                curr_val = self.get_action_alpha_beta(game_state.generate_successor(0, action), curDepth, False, alpha,
+                                                      beta)
+                max_val = max(curr_val, max_val)
+                alpha = max(alpha, max_val)
                 if beta <= alpha:
                     break
-            return best_value
+
+            return max_val
 
         else:
-            return self.get_action_alpha_beta(game_state.generate_successor(1), curDepth, True, alpha, beta)
+            legal_moves = game_state.get_opponent_legal_actions()
+            min_val = np.float('inf')
+            for action in legal_moves:
+                curr_val = self.get_action_alpha_beta(game_state.generate_successor(1, action), curDepth+1, True, alpha,
+                                                      beta)
+                min_val = min(curr_val, min_val)
+                beta = min(beta, min_val)
+                if beta <= alpha:
+                    break
 
+            return min_val
 
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
@@ -228,6 +266,7 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         """
         """*** YOUR CODE HERE ***"""
         util.raiseNotDefined()
+
 
 
 def better_evaluation_function(current_game_state):
@@ -254,15 +293,18 @@ def better_evaluation_function(current_game_state):
 
     """
     "*** YOUR CODE HERE ***"
-    board = current_game_state.board
 
+
+    board = current_game_state.board
+    # if np.count_nonzero(board) == 16:
+    #     return -np.inf
     # the symmetric formation we have:
     scores1 = np.array([[6, 5, 4, 3], [5, 4, 3, 2], [4, 3, 2, 1], [3, 2, 1, 0]])
     scores2 = np.array([[3, 4, 5, 6], [2, 3, 4, 5], [1, 2, 3, 4], [0, 1, 2, 3]])
     scores3 = np.array([[3, 2, 1, 0], [4, 3, 2, 1], [5, 4, 3, 2], [6, 5, 4, 3]])
     scores4 = np.array([[0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6]])
 
-    scores = [0, 1, 1, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 5, 5, 6]
+    scores = [0,1,1,2,2,2,3,3,3,3,4,4,4,5,5,6]
 
     # calculating the max symmetric score:
     snake = 0
@@ -277,15 +319,9 @@ def better_evaluation_function(current_game_state):
     snake_score = snake / max_possible
 
     # calculating the percentage of free tiles:
-    free_tiles_factor = 15 - np.log2(current_game_state.max_tile)
+    free_tiles_factor = 15 - np.unique(current_game_state.board).shape[0]
     free_tiles_score = (16 - np.count_nonzero(board)) / free_tiles_factor
 
-    # adding the "max tile to the corner" bonus
-    max_bonus = 0
-    if max([board[0,0], board[0,3], board[3,0], board[3,3]]) == current_game_state.max_tile:
-        max_bonus = 1
-    return snake_score + free_tiles_score + max_bonus*2
-
-
+    return 5*snake_score + free_tiles_score
 # Abbreviation
 better = better_evaluation_function
